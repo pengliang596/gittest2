bcb8c8d044720562219f3d409917b5f0c9d884ac
pengliangxp <pengliangxp@126.com>
2019-10-16 11:57:15 +0800

    上传java工程

---
+++ gittest2/src/cn/haofu7/test2.java
@@ -0,0 +1,9 @@
+package cn.haofu7;
+
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+public class test2 {
+    public static void main(String[] args) {
+        System.out.print("pengliang596");
+    }
+}
